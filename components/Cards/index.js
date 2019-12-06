4b6b2a1131d19e12ad9bf34c37b2216122a30391
agohorel <gohorelalex@gmail.com>
2019-12-06 11:47:51 -0500

    add card creator functions

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,48 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+function createCard(card) {
+  const component = document.createElement("div");
+  component.classList.add("card");
+
+  const headline = document.createElement("div");
+  headline.classList.add("headline");
+  headline.textContent = card.headline;
+  component.appendChild(headline);
+
+  const author = document.createElement("div");
+  author.classList.add("author");
+  component.appendChild(author);
+
+  const imgContainer = document.createElement("div");
+  imgContainer.classList.add("img-container");
+  author.appendChild(imgContainer);
+
+  const img = document.createElement("img");
+  img.src = card.authorPhoto;
+  imgContainer.appendChild(img);
+
+  const byline = document.createElement("span");
+  byline.textContent = `By ${card.authorName}`;
+  author.appendChild(byline);
+
+  return component;
+}
+
+function getCards() {
+  axios
+    .get("https://lambda-times-backend.herokuapp.com/articles")
+    .then(res => {
+      Object.keys(res.data.articles).forEach(subject => {
+        res.data.articles[subject].map(article => {
+          document
+            .querySelector(".cards-container")
+            .appendChild(createCard(article));
+        });
+      });
+    })
+    .catch(err => console.error(err));
+}
+
+getCards();
